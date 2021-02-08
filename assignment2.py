5f938fdd9b70a4d229ebf8cfe0ef1e7b9f156d46
Lj <55156461+Usercodehappen@users.noreply.github.com>
2021-02-08 10:22:51 -0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 fname1 = "Mr"
 fname2 = "Yang"
 print(fname1)
 print(fname2)
 
 # fnames1 defines "Mr" while fname 2 defines Yang as other one 
+=======
+
+>>>>>>> 992102932f27deeb2a5f5b59b5a5167780379ae8
