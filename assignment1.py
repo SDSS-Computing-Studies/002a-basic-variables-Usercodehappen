7dc369938005bb6e61175e82479ff54e7f6108fb
Lj <55156461+Usercodehappen@users.noreply.github.com>
2021-02-08 10:20:57 -0800

    update

--- assignment1.py
+++ assignment1.py
@@ -9,17 +9,5 @@ print(y)
 
 # x = 10 variable is present  # y = 2.4 variable is present
 
-fname1 = "Mr"
-fname2 = "Yang"
-print(fname1)
-print(fname2)
 
-# fnames1 defines "Mr" while fname 2 defines Yang as other one 
 
-fname = "10"
-lname = 10
-lname =str(lname)
-print(fname)
-print(lname)
-
-#fname defines "fname" as a name 10 while variable lname defines as 10 str variables are strings(lname)
---
+++ assignment3.py
@@ -0,0 +1,7 @@
+fname = "10"
+lname = 10
+lname =str(lname)
+print(fname)
+print(lname)
+
+#fname defines "fname" as a name 10 while variable lname defines as 10 str variables are strings(lname)
