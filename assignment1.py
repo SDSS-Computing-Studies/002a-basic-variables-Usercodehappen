bbdf006798ad4717d6210380dd3969ab734fd188
Lj <55156461+Usercodehappen@users.noreply.github.com>
2021-02-08 10:10:22 -0800

    update

--- assignment1.py
+++ assignment1.py
@@ -3,10 +3,23 @@
 # SD Computing Studies Assignment
 
 x = 10
-y = 2.5
+y = 2.4
 print(x)
 print(y)
 
+# x = 10 variable is present  # y = 2.4 variable is present
 
+fname1 = "Mr"
+fname2 = "Yang"
+print(fname1)
+print(fname2)
 
+# fnames1 defines "Mr" while fname 2 defines Yang as other one 
 
+fname = "10"
+lname = 10
+lname =str(lname)
+print(fname)
+print(lname)
+
+#fname defines "fname" as a name 10 while variable lname defines as 10 str variables are strings(lname)
